aabe28c59494d6cb6468f3e80683723d158066d0
Anythingpgv <aryelchristian102@gmail.com>
2025-03-23 23:56:18 -0300

    Add files via upload

---
+++ Tarefas.js
@@ -0,0 +1,7 @@
+const tarefas = ["Comprar", "Limpar", "Lavar", "Estudar"]
+
+function gerenciarTarefas() {
+    const tarefas = ["Comprar", "Limpar", "Lavar", "Estudar"]
+    return tarefas.push("Correr")
+}
+console.log(gerenciarTarefas(tarefas))
